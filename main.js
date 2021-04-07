6d7b7041e7faa516c022a2d58002721947debd20
Samiksha123-creator <81385822+Samiksha123-creator@users.noreply.github.com>
2021-04-07 15:02:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+
+ <head>
+   <title> My favourite Hobby </title> </head>
+<link rel= "stylesheet" href= "style.css">   
+<body>
+ <h1 id= h1> My Favourite Hobby </h1>
+ <ul class = "menu">
+    <li class = "links"><a href= "#welcome">Home</a></li>
+ <li class = "links"><a href= "#videos">My Videos</a></li>
+ <li class = "links"><a href= "#Work">Work</a></li>
+ <li class = "links"><a href= "#Writing">Writings</a></li>
+ <li class = "links"><a href= "#contacts">Contacts</a></li>
+
+</ul> 
+ <img id=img1 src=
+"https: //images.unsplash.com/photo-1547748796-f3279852175f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYxNjQyNjM0OQ&ixlib=rb-1.2.1&q=80&w=400">
+<p> "My favourite Hobby is playing the violin. I love playing the violin because it is very calming and has a beautiful sound to it. It is very hard to play but practicing helps. I am currently learning the carnatic violin and I love it. Carnatic violin as this attraction which all of this complexity which I hope to acheve one day. Therefore I love playing the Violin and will not give it up." </p
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
+</body 
